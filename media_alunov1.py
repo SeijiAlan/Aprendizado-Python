39f357200a4331594d0c6e6254578a924a2beab3
SeijiAlan <emprego.seiji@gmail.com>
2025-11-07 23:59:24 -0300

    Adds comments about how the script works and some data validation mechanisms

--- media_alunov1.py
+++ media_alunov1.py
@@ -1,23 +1,29 @@
 alunos=[]
+#Função que cadastra os dados inseridos
 def cadastro_alunos():
     while True:
-        nome_aluno=str (input('Digite o nome do aluno: '))
+        nome_aluno=str (input('Digite o nome do aluno: ')).strip().lower()
+        #Metodo de validação, permite apenas letras
         if not nome_aluno.replace(' ',"").isalpha():
             print('Digite apenas letras')
             continue
         else:
             break
 
+    #Toda vez que um novo cadastro for iniciado a lista notas será zerada.
     notas=[]
     for c in range(2):
         nota=float(input(f'Digite a nota {c+1}'))
         notas.append(nota)
 
-
+    #sum e len são comandos utilizados para realizar operações de dados/valores que se encontram em uma lista
     media=sum(notas)/len(notas)
+
+    #A lista "alunos" é a principal sendo que a lista "notas" foi englobada
     alunos.append({'nome':nome_aluno,'notas':notas,'media':media})
     continuar()
 
+#Função que contém loop while,independentemente da resposta o programa volta ao menu (via return), dessa forma é possível escolher o que fazer
 def continuar():
     while True:
         cont=str(input('Deseja cadastrar outro aluno (a)?')).lower().strip()
@@ -31,24 +37,29 @@ def continuar():
             print('Resposta inválida !')
             continue
 
+#Função criada para permitir a consulta das notas de um aluno em especifico
 def notas_individuais():
     while True:
-        nome_individual=str(input('Digite o nome do aluno para visualizar suas notas: '))
+        nome_individual=str(input('Digite o nome do aluno para visualizar suas notas: ')).lower().strip()
         if not nome_individual.replace(' ',"").isalpha():
             print('Digite apenas Letras !')
             continue
 
-        alunos_existentes=[p['nome'].lower() for p in alunos]
+        #Variavel que guarda o acesso ao nome de cada aluno na lista alunos
+        alunos_existentes=[p['nome'].lower().strip() for p in alunos]
+
+        #Se o nome fornecido não estiver no cadastro o programa retorna ao menu depois de apresentar a mensagem do print
         if nome_individual not in alunos_existentes:
             print('Aluno não encontrado!')
             return
 
+        #Percorre aluno por aluno na lista alunos até achar o nome fornecio em "nome_individual"), quando acha printa as informações e sai da função
         for aluno in alunos:
-            if aluno['nome'].lower() == nome_individual:
+            if aluno['nome'].lower().strip() == nome_individual:
                 print(f"Nome do aluno:{aluno['nome']}, Notas: {aluno['notas']}")
                 return
 
-
+#Função que mostra a lista com nome e média de todos os alunos
 def media_todos():
     if not alunos:
         print('Nenhum aluno cadastrado!')
@@ -59,7 +70,7 @@ def media_todos():
 
 
 
-
+#Função menu interativo
 def menu():
     while True:
         try:
